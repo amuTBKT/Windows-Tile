800da137a7364f37afa46416dbe04c92948a0a82
Amit Kumar Mehar <gookutbkt@gmail.com>
2014-05-11 18:56:10 +0530

    windows tile initial plugin
    removes ios plugin

--- js/tile.js
+++ js/tile.js
@@ -1,97 +1,84 @@
-(function(){
+;(function($){
 
-	var tileWidth = 200, tileHeight = 200;
+	var config;
 
-	var tile = document.getElementsByClassName("tile multiple")[0];
-
-	var tileS = document.getElementsByClassName("tile single")[0];
-
-	var imageSArray = ["images/Tiles/stile.png", "images/Tiles/sstile.png"];
-	var imageSSArray = ["images/Tiles/sstile0.png", "images/Tiles/sstile1.png", 
-						"images/Tiles/sstile2.png", "images/Tiles/sstile3.png", "images/Tiles/sstile.png"];
-
-	var imageArray = ["images/Tiles/tile1.png", "images/Tiles/tile2.png", "images/Tiles/tile3.png", "images/Tiles/tile4.png"];
-
-	var imageSMArray = ["images/Tiles/sstile0.png", "images/Tiles/sstile1.png", "images/Tiles/sstile2.png", "images/Tiles/sstile3.png"];
-
-	var tileObject = new Tile(tile);
-	tileObject.loadTiles(imageArray);
-
-	var tileObjectS = new Tile(tileS);
-	tileObjectS.loadTiles(imageSArray);
-
-	tileS.onclick = function(e){
-		tileObjectS.loadTiles(imageSSArray);
-		tileObject.loadTiles(imageSMArray);
+	var defaults = {
+		type: "single",
+		delay: 1000
 	}
 
-	function Tile(tile){
-		this.tile = tile;
-		this.images = this.tile.getElementsByClassName("image")[0];
-		this.imageFigure = this.images.getElementsByTagName('figure');
+	function Tile(element, options){
+		config = $.extend({}, defaults, options);
 
+		this.element = element;
 		this.mode = [];
-
-		this.firstImage = this.imageFigure[0];
-		this.secondImage = this.imageFigure[1];
-		if (this.secondImage != null){
-			this.isSingle = false;
-			this.thirdImage = this.imageFigure[2];
-			this.fourthImage = this.imageFigure[3];
-			this.tileArray = [this.firstImage, this.secondImage, this.thirdImage, this.fourthImage];
-			this.mode[0] = "back";
-			this.mode[1] = "back";
-			this.mode[2] = "back";
-			this.mode[3] = "back";
-		}
-		else {
-			this.isSingle = true;
-			this.mode[0] = "back";
-			this.tileArray = [this.firstImage];
-		}
-
+		this.tileArray = [];
+		this.isSingle = true;
 		this.timer = null;
 		this.position = -1;
+		
+		this.tileArray = [];
+
+		this.tileContentArray = [];
+
+		this.init = function(){
+			if (config.type == "multi"){
+				var classArray = ["first", "second", "third", "fourth"];
+				this.element.addClass('tile').addClass('multiple');
+				this.isSingle = false;
+				this.firstTileContent = [];this.secondTileContent = [];this.thirdTileContent = [];this.fourthTileContent = [];
+				this.tileContentArray[0] = this.firstTileContent;this.tileContentArray[1] = this.secondTileContent;this.tileContentArray[2] = this.thirdTileContent;this.tileContentArray[3] = this.fourthTileContent;
+			}
+			else {
+				var classArray = ["first"];
+				this.element.addClass('tile').addClass('single');
+				this.firstTileContent = [];
+				this.tileContentArray[0] = this.firstTileContent;
+			}
+
+			var imageDiv = $('<div/>').addClass('image').appendTo(this.element);
+
+			for (var i = 0; i < classArray.length; i++){
+				var fig = $('<figure/>').addClass(classArray[i]).appendTo(imageDiv);
+				var front = $('<div/>').addClass('front').appendTo(fig);
+				var back = $('<div/>').addClass('back').appendTo(fig);
+				this.mode[i] = "back";
+				this.tileArray[i] = fig;
+				this.tileContentArray[i][0] = front;
+				this.tileContentArray[i][1] = back;
+			}
+		}
 
 		this.setTiles = function(frontImage, backImage){
-			var i;
-			for (i = 0; i < this.tileArray.length; i++){
+			for (var i = 0; i < this.tileContentArray.length; i++){
 				if (frontImage){
-					var front = this.tileArray[i].getElementsByClassName("front")[0];
-					front.style.backgroundImage = "url(" + frontImage[i] + ")";
+					var front = this.tileContentArray[i][0];
+					front.css('background-image', "url(" + frontImage[i] + ")");
 				}
 
 				if (backImage){
-					var back = this.tileArray[i].getElementsByClassName("back")[0];
-					back.style.backgroundImage = "url(" + backImage[i] + ")";
+					var back = this.tileContentArray[i][1];
+					back.css('background-image', "url(" + backImage[i] + ")");
 				}
 			}
 		}
 
 		this.loadTiles = function(imageArray){
 			if (!this.isSingle){
-				this.position = loadTiles(this.tileArray, imageArray, this.mode, this.position, this.timer, 500 + 500 * (0.5 - 0.5 * Math.random()));
+				this.position = loadTiles(this.tileArray, this.tileContentArray, imageArray, this.mode, this.position, this.timer);
 			}
 			else {
-				loadTile(this.tileArray[0], imageArray, this.mode, -1, this.timer, 500 + 500 * (0.5 - 0.5 * Math.random()));
+				loadTiles(this.tileArray, this.tileContentArray, imageArray, this.mode, -1, this.timer);
 			}
 		}
 
 		this.loadTile = function(tileNo, image, time){
 			loadTile(this.tileArray[tileNo], image, this.mode, -1, this.timer, time);
 		}
-
-		this.getTileArray = function(){
-			return this.tileArray;
-		}
-	}
-
-	function logg(message){
-		console.log("" + message);
 	}
 
 	// for multi-image tile
-	function loadTiles(tileArray, imageArray, mode, pos, timer, delay){
+	function loadTiles(tileArray, tileContentArray, imageArray, mode, pos, timer){
 		var counter = pos;
 		if (timer){
 			clearTimeout(timer);
@@ -103,14 +90,14 @@
 			mode[counter % tileArray.length] = flipMode(mode[counter % tileArray.length]);
 			timer = setInterval(function(){
 				if (mode[counter % tileArray.length] == "front"){
-					flipFront(tileArray[counter % tileArray.length], imageArray[counter]);
+					flipFront(tileArray, tileContentArray, imageArray[counter], counter % tileArray.length);
 				}
 				else {
-					flipBack(tileArray[counter % tileArray.length], imageArray[counter]);
+					flipBack(tileArray, tileContentArray, imageArray[counter], counter % tileArray.length);
 				}
 
-				loadTiles(tileArray, imageArray, mode, counter, timer, 2500 + 500 * (0.5 - 0.5 * Math.random()));
-			}, delay);
+				loadTiles(tileArray, tileContentArray, imageArray, mode, counter, timer);
+			}, config.delay);
 		}
 
 		else {
@@ -122,35 +109,6 @@
 		return position;
 	}
 
-	// for single image tile
-	function loadTile(tile, imageArray, mode, pos, timer, delay){
-		var counter = pos;
-		if (timer){
-			clearTimeout(timer);
-		}
-
-		if (counter < imageArray.length - 1){
-			counter ++;
-		}
-		else {
-			clearTimeout(timer);
-			return false;
-		}
-
-		mode[0] = flipMode(mode[0]);
-
-		timer = setInterval(function(){
-			if (mode[0] == "front"){
-				flipFront(tile, imageArray[counter]);
-			}
-			else {
-				flipBack(tile, imageArray[counter]);
-			}
-			loadTile(tile, imageArray, mode, counter, timer, 2500 + 500 * (0.5 - 0.5 * Math.random()));
-
-		}, delay);
-	}	
-
 	function flipMode(mode){
 		if (mode == "back"){
 			mode = "front";
@@ -161,25 +119,41 @@
 		return mode;
 	}
 
-	function flipFront(imageTile, background){
+	function flipFront(tileArray, tileContentArray, background, counter){
 		if (background){
-			var back = imageTile.getElementsByClassName("back")[0];
-			back.style.backgroundImage = "url(" + background + ")";
+			var back = tileContentArray[counter][1];
+			back.css('background-image', "url(" + background + ")");
 		}
-		imageTile.classList.remove("rotateXX");
-		imageTile.classList.add("rotateX");
+		tileArray[counter].removeClass("rotateXX");
+		tileArray[counter].addClass("rotateX");
 	}
 
-	function flipBack(imageTile, background){
+	function flipBack(tileArray, tileContentArray, background, counter){
 		if (background){
-			var front = imageTile.getElementsByClassName("front")[0];
-			front.style.backgroundImage = "url(" + background + ")";
+			var front = tileContentArray[counter][0];
+			front.css('background-image', "url(" + background + ")");
 		}
-		imageTile.classList.remove("rotateX");
-		imageTile.classList.add("rotateXX");
+		tileArray[counter].removeClass("rotateX");
+		tileArray[counter].addClass("rotateXX");
 	}
 
-}());
+	$.fn.initTile = function(options){
+		tile = new Tile(this, options);
+		tile.init();
+		return this.first();
+	};
+
+	$.fn.setTile = function(frontImageArray, backImageArray){
+		tile.setTiles(frontImageArray, backImageArray);
+		return this.first();
+	};
+
+	$.fn.loadTile = function(imageArray){
+		tile.loadTiles(imageArray);
+		return this.first();
+	};
+
+}(jQuery));
 
 
 
